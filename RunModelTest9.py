310f2ef49dc073dfe74542b43a36bdb6598e5dc7
mstef4no <mattia.stefano@cnr.it>
2024-12-13 11:22:30 +0100

    Add files via upload

---
+++ RunModelTest9.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+
+from Test_9_p2b import NetworkModel
+
+sigma_arr = np.array([0.1, 0.2,  0.5, 1, 10]) 
+alpha_arr = np.array([0.01, 0.1, 0.15, 0.2, 1]) 
+
+for i in range(len(sigma_arr)): 
+    for j in range(len(alpha_arr)): 
+
+        
+        sigma = sigma_arr[i]; 
+        alpha = alpha_arr[j]
+        print(alpha,sigma)
+        exec(open("./Test_9_p2b.py").read()) 
+       
+        NetworkModel(sigma, alpha)

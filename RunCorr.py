5c5a56c070cef3043ae156b5dc4bd9d9676dbc99
mstef4no <mattia.stefano@cnr.it>
2025-04-28 15:58:12 +0200

    Add files via upload

--- RunCorr.py
+++ RunCorr.py
@@ -1,13 +1,16 @@
-import numpy as np
 
-sigma_arr = np.array([0.1, 0.2,  0.5, 1, 10]) 
-alpha_arr = np.array([0.01, 0.1, 0.15, 0.2, 1]) 
+import numpy as np
+ 
+sigma_arr = np.array([0.1, 0.2,  0.5, 1.0, 10.0]) 
+alpha_arr = np.array([0.01, 0.1, 0.15, 0.2, 1.0]) 
+
+
 
 for i in range(len(sigma_arr)): 
     for j in range(len(alpha_arr)): 
-
+    
         sigma = sigma_arr[i]; 
         alpha = alpha_arr[j]
-        print(sigma,alpha)
+        print(sigma, alpha)
         exec(open("./MainCorr.py").read()) 
         NetworkCorr(sigma, alpha)

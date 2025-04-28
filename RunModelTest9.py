fd5644cb6625f441484468568490d269084ff6fb
mstef4no <mattia.stefano@cnr.it>
2025-04-28 16:00:12 +0200

    Add files via upload

---
+++ RunModelTest9.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+sigma_arr = np.array([0.1, 0.2,  0.5, 1, 10.0]) 
+alpha_arr = np.array([0.01, 0.1, 0.15, 0.2, 1])  
+
+for i in range(len(sigma_arr)): 
+    for j in range(len(alpha_arr)): 
+
+        sigma = sigma_arr[i]; 
+        alpha = alpha_arr[j]
+ 
+       	NetworkModel(sigma, alpha)
+        
